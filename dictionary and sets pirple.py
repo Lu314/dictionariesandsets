417747690077e313c3dbcf05065bfd70bf7f8106
Lucas Scalzo <l.scalzo90@gmail.com>
2020-12-12 13:17:56 -0600

    Add files via upload

---
+++ dictionary and sets pirple.py
@@ -0,0 +1,17 @@
+
+Favoritesong = {
+    'genre': 'rap', 
+    'name': 'Dead presidents two',
+    'artist': 'Jay Z',
+    'released': '1996', 
+    'recorded': '1995', 
+     'label': 'Roc-A-Fella',
+     'songwriter': 'Shawn Carter', 
+     'producers': 'Ski', 
+     'DurationInSeconds': '266 seconds', 
+     'sample': 'Lonnie Liston Smith,(A Garden of Peace)', 
+     'certification': 'Gold'
+}
+
+for key, value in Favoritesong.items():
+    print(key, value)
